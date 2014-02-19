286668b972f71e08a8f3f72a4d24ed7d83c538c1
openwrt <jesstr@mail.ru>
2014-02-19 19:11:53 +0400

    speed control aded

--- linux-wifibot-client.c
+++ linux-wifibot-client.c
@@ -13,6 +13,8 @@
 
 #define MAX_COMMAND_LENGTH	16
 #define CENTER_STEER_POS	850
+#define SPEED_CHANGE_DELAY_US	100000
+#define COMMAND_SEND_DELAY_US	500000
 
 char command1[MAX_COMMAND_LENGTH];	/* Independent command buffers for safe using in threads */
 char command2[MAX_COMMAND_LENGTH];
@@ -21,26 +23,33 @@ char command3[MAX_COMMAND_LENGTH];	/* Command buffer for using in non-thread fun
 
 unsigned char run_speed = 200;	/* Default forward and backward run speed value, percent */
 unsigned char run_time = 20; 	/* Default forward and backward run time value, 1=100ms */
+char *run_direction = "F";		/* Default forward or backward run direction, "F" or "B" */
 unsigned short steer_pos = CENTER_STEER_POS;	/* Default steering servo position, us */
 
 SDL_Event event;	/* The event structure */
 int sock;
 
 
-void * thread_func(void *arg)
+void * thread_command_send(void *arg)
 {
 	char *command;
 	unsigned char id = * (unsigned char *) arg;
 	Uint8 *keystate = SDL_GetKeyState( NULL );
 
-	switch ( id ) {
-		case 1: command = command1; break;
-		case 2: command = command2; break;
-	}
 	do {
+		switch ( id ) {
+			case 1:
+				sprintf(command1, "run=%s,%d,%d\n", run_direction, run_speed, run_time);
+				command = command1;
+				break;
+			case 2:
+				sprintf(command2, "run=%s,%d,%d\n", run_direction, run_speed, run_time);
+				command = command2;
+				break;
+		}
 		puts("sent");
 		send(sock, command, strlen(command), 0);
-		usleep(500000);
+		usleep(COMMAND_SEND_DELAY_US);
 		/* Exit thread if key was released*/
 		if ( ( !(keystate[SDLK_UP]) && id == 1) ||
 			 ( !(keystate[SDLK_DOWN]) && id == 2) )
@@ -50,9 +59,29 @@ void * thread_func(void *arg)
 	pthread_exit(NULL);
 }
 
+void * thread_speed_change(void *arg)
+{
+	Uint8 *keystate = SDL_GetKeyState( NULL );
+
+	do {
+	if ( keystate[SDLK_PAGEUP] )
+		if ( run_speed < 255 ) {
+			run_speed++;
+			usleep(SPEED_CHANGE_DELAY_US);
+		}
+	if ( keystate[SDLK_PAGEDOWN] )
+		if ( run_speed > 0 ) {
+			run_speed--;
+			usleep(SPEED_CHANGE_DELAY_US);
+		}
+	}
+	while ( keystate[SDLK_PAGEUP] || keystate[SDLK_PAGEDOWN] );
+	pthread_exit(NULL);
+}
+
 int main(int argc, char **argv)
 {
-	pthread_t thread1;
+	pthread_t thread1, thread2, thread3;
 	unsigned char thread_id;
 	struct sockaddr_in addr;
 	int result;
@@ -75,15 +104,14 @@ int main(int argc, char **argv)
     //addr.sin_addr.s_addr = inet_addr("192.168.1.121");
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     
-    printf("Connecting...\n");
+    puts("Connecting...");
     
     if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("connect");
         return 2;
     }
 
-	printf("Connected.\n");
-
+    puts("Connected.");
 	puts("press ENTER to exit");
 
 	while(1) {
@@ -93,20 +121,22 @@ int main(int argc, char **argv)
 	   			//Выбрать правильное сообщение
 	            switch( event.key.keysym.sym ) {
 	                case SDLK_UP:
-	                	sprintf(command1, "run=%s,%d,%d\n", "F", run_speed, run_time);
+	                	run_direction = "F";
+	                	sprintf(command1, "run=%s,%d,%d\n", run_direction, run_speed, run_time);
 	                	puts("UP");
 	                	thread_id = 1;
-	                	result = pthread_create(&thread1, NULL, thread_func, &thread_id);
+	                	result = pthread_create(&thread1, NULL, thread_command_send, &thread_id);
 	                	if (result != 0) {
 	                		perror("Creating the thread");
 	                		return 1;
 	                	}
 	                	break;
 	                case SDLK_DOWN:
-	                	sprintf(command2, "run=%s,%d,%d\n", "B", run_speed, run_time);
+	                	run_direction = "B";
+	                	sprintf(command2, "run=%s,%d,%d\n", run_direction, run_speed, run_time);
 	                	puts("DOWN");
 	                	thread_id = 2;
-	                	result = pthread_create(&thread1, NULL, thread_func, &thread_id);
+	                	result = pthread_create(&thread2, NULL, thread_command_send, &thread_id);
 	                	if (result != 0) {
 	                		perror("Creating the thread");
 	                		return 1;
@@ -126,8 +156,12 @@ int main(int argc, char **argv)
 	                	puts("RIGHT");
 	                	break;
 	                case SDLK_PAGEUP:
-	                	break;
 	                case SDLK_PAGEDOWN:
+	                	result = pthread_create(&thread3, NULL, thread_speed_change, NULL);
+	                	if (result != 0) {
+	                		perror("Creating the thread");
+	                		return 1;
+	                	}
 	                	break;
 	                case SDLK_RETURN:
 	                	//Выходим из программы
@@ -155,6 +189,14 @@ int main(int argc, char **argv)
 	                	send(sock, command3, strlen(command3), 0);
 	                	puts("CENTER");
 	                	break;
+	                case SDLK_PAGEUP:
+	                case SDLK_PAGEDOWN:
+	                	result = pthread_cancel(thread3);
+	                	if (result != 0) {
+	                		perror("Cancelling the thread");
+	                		return 1;
+	                	}
+	                	break;
 	                default:
 	                	break;
 	            }
@@ -168,5 +210,5 @@ int main(int argc, char **argv)
 	        }
 	    }
 	}
-return EXIT_SUCCESS;
+return 0;
 }
